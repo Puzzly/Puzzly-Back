6c65ba09fc2eed4c8dfae515db6be935819fe70c
GGooks <luvrkdus@gmail.com>
2024-02-14 14:28:01 +0900

    :triangular_flag_on_post: Create project

---
+++ src/main/java/com/puzzly/PuzzlyApplication.java
@@ -0,0 +1,13 @@
+package com.puzzly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuzzlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/puzzly/PuzzlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puzzly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuzzlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
