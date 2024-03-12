e8f774abf609ebc89418d86f0b987635969a2188
GGooks <luvrkdus@gmail.com>
2024-03-12 21:51:32 +0900

    :white_check_mark: Modify Calendar function

---
+++ src/main/java/com/puzzly/dto/CalReq.java
@@ -0,0 +1,13 @@
+package com.puzzly.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class CalReq {
+
+    private String calId;
+    private String email;
+
+}
