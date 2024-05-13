8921a8507ffbc86b76b705ab32d0306ff2c94289
junhun.lee <hkglh12@gmail.com>
2024-05-13 14:13:42 +0900

    feature/file upload seperated from calendarContentsAPI, api opened

--- src/main/java/com/puzzly/api/repository/mybatis/CalendarContentsMybatisRepository.java
+++ src/main/java/com/puzzly/api/repository/mybatis/CalendarContentsMybatisRepository.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,7 +14,7 @@ public interface CalendarContentsMybatisRepository {
 
     //
     public List<CalendarContentsResponseDto> selectCalendarContentsByStartDateTimeAndCalendar(@Param("calendarId") Long calendarId, @Param("startDateTime") LocalDateTime startDateTime, @Param("limitStartDateTime") LocalDateTime limitStartDateTime, @Param("isDeleted") boolean isDeleted);
-    public List<Map<String, Object>> selectCalendarContentsAttachmentsByContentsId(@Param("contentsId") Long contentsId);
+    public List<HashMap<String, Object>> selectCalendarContentsAttachmentsByContentsId(@Param("contentsId") Long contentsId);
 
     public CalendarContentsResponseDto selectCalendarContentsByContentsId(@Param("contentsId") Long contentsId);
 }
