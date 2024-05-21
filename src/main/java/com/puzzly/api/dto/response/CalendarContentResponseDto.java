43483e4fe8a39231027205a45a0d3f12242048bc
KimSori <noksm2@naver.com>
2024-05-21 09:21:30 +0900

    refactor/calendar content api -> label CRU add (#47)
    1. 캘린더 일정 등록, 수정, 리스트 API label 추가

--- src/main/java/com/puzzly/api/dto/response/CalendarContentResponseDto.java
+++ src/main/java/com/puzzly/api/dto/response/CalendarContentResponseDto.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
 
 @Builder
@@ -22,9 +21,12 @@ public class CalendarContentResponseDto {
     @Schema(description = "캘린더 이름")
     private String calendarName;
 
-    //TODO Label RequqestDTO,RESPONSE DTO
     @Schema(description = "일정이 가질 Label ID, 생략할 수 있음, 단일 컨텐츠는 단일 라벨 아이디만 가질 수 있음", defaultValue = "0")
     private Long labelId;
+    @Schema(description = "라벨 이름")
+    private String labelName;
+    @Schema(description = "라벨 색상")
+    private String colorCode;
 
     @Schema(description = "일정 제목")
     private String title;
