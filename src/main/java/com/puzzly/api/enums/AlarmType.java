b345dc1d349cbabd33ddb63b343a1a1277705119
KimSori <noksm2@naver.com>
2024-06-15 23:00:32 +0900

    refactor/CalendarContent AlarmType change

--- src/main/java/com/puzzly/api/enums/AlarmType.java
+++ src/main/java/com/puzzly/api/enums/AlarmType.java
@@ -2,10 +2,8 @@ package com.puzzly.api.enums;
 
 public enum AlarmType {
   NONE,
-  SAME_TIME,
-  TEN_MINUTES_BEFORE,
-  ONE_HOUR_BEFORE,
-  ONE_DAY_BEFORE,
-  CUSTOM
+  MINUTE,
+  HOUR,
+  DAY
 
 }
